61e548b542703ac449fe02e2de916a8f5ef0b080
KUMA-PC\bearhsu2 <bearhsu2@gmail.com>
2018-04-02 23:52:30 +0800

    temp

--- src/main/java/idv/kuma/_4kyu/integer_partitions/IntPart.java
+++ src/main/java/idv/kuma/_4kyu/integer_partitions/IntPart.java
@@ -7,25 +7,39 @@ import java.util.List;
  * Created by bearhsu2 on 4/2/2018.
  */
 //https://stackoverflow.com/questions/32907406/is-there-an-efficient-algorithm-for-integer-partitioning-with-restricted-number
+//https://stackoverflow.com/questions/44359310/integer-partitioning-in-java
+//https://introcs.cs.princeton.edu/java/23recursion/Partition.java.html
 public class IntPart {
 
     public static String part(long n) {
-        // your code
-        List<List<Long>> partitions = findPartitions(n);
+        // 1
+        List<List<Long>> partitions = generatePartitions(n);
 
+        // 2
+
+        // 3
+
+        // format to string return
         return partitionToString();
     }
 
-    private static List<List<Long>> findPartitions(long n) {
+    private static List<List<Long>> generatePartitions(long n) {
         List<List<Long>> result = new ArrayList<>();
-        for (int m = 1; m <= n; m++) {
-            result.addAll(findSubPartitions(n, m));
-        }
+
+        result = findSubPartitions(n, n, result);
+
         return result;
     }
 
-    private static List<List<Long>> findSubPartitions(long n, long m) {
-        List<List<Long>> result = new ArrayList<>();
+    private static List<List<Long>> findSubPartitions(long n, long max, List<List<Long>> result) {
+        if (n == 0L) {
+            return null;
+        }
+
+        for (int i = (int) Math.min(max, n); i >= 1; i--) {
+            findSubPartitions(n - i, i, prefix + " " + i);
+        }
+
         return result;
     }
 
