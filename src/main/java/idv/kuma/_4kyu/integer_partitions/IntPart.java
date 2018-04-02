b2c22ed6294a5063b3923bb6bdfccbd5c19cd7c0
KUMA-PC\bearhsu2 <bearhsu2@gmail.com>
2018-04-02 23:24:15 +0800

    init

---
+++ src/main/java/idv/kuma/_4kyu/integer_partitions/IntPart.java
@@ -0,0 +1,11 @@
+package idv.kuma._4kyu.integer_partitions;
+
+/**
+ * Created by bearhsu2 on 4/2/2018.
+ */
+public class IntPart {
+
+    public static String part(long n) {
+        // your code
+    }
+
---
+++ src/test/java/idv/kuma/_4kyu/integer_partitions/IntPartTest.java
@@ -0,0 +1,15 @@
+package idv.kuma._4kyu.integer_partitions;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IntPartTest {
+
+    @Test
+    public void Numbers_Small() {
+        assertEquals("Range: 1 Average: 1.50 Median: 1.50", IntPart.part(2));
+        assertEquals("Range: 2 Average: 2.00 Median: 2.00", IntPart.part(3));
+        assertEquals("Range: 3 Average: 2.50 Median: 2.50", IntPart.part(4));
+        assertEquals("Range: 5 Average: 3.50 Median: 3.50", IntPart.part(5));
+    }
+}
