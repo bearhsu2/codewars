231ff3a66cf3146658135fa0a72e987ff8ecb662
Yu-Song Syu <kuma@simbo.com.tw>
2018-04-08 15:05:40 +0800

    4 kyu: ten-pin bowling

--- src/main/java/idv/kuma/_4kyu/ten_pin_bowling/Solution.java
+++ src/main/java/idv/kuma/_4kyu/ten_pin_bowling/Solution.java
@@ -8,38 +8,96 @@ public class Solution {
         String[] frames = rawData.split(" ");
 
         for (int i = 0; i < frames.length; i++) {
-
             totalScore += handleFrame(i, frames);
-
         }
 
+        System.out.println("===============================");
+        System.out.println("Total = " + totalScore);
+        System.out.println("============================================");
         return totalScore;
+
     }
 
     public static int handleFrame(int idx, String[] frames) {
+        System.out.println("===============================");
 
         String frame = frames[idx];
 
         if ("X".equalsIgnoreCase(frame)) {
+            System.out.println("Frame " + idx + " is strike: X");
             return handleFrameStrike(idx, frames);
         }
 
         if (frame.endsWith("/")) {
-            return 0;
+            System.out.println("Frame " + idx + " is spare: " + frame);
+            return handleFrameSpare(idx, frames);
         }
 
-        if (idx >= 10) {
-            // TODO: 10th frame, special form
-            return 0;
+        if (idx >= 9) {
+            System.out.println("Frame " + idx + " is last frame: " + frame);
+            return handleLastFrame(frame);
         }
 
         // oridinary frame
+        System.out.println("Frame " + idx + " is ordinary frame: " + frame);
         return handleFrameOrdinary(frame);
 
     }
 
-    public static int handleFrameStrike(int idx, String[] frames){
-        return 0;
+    public static int handleLastFrame(String frame) {
+        int result = 0;
+
+        char[] charArray = frame.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            result += getRollRawScore(i == 0 ? '0' : charArray[i - 1], charArray[i]);
+            System.out.println("roll #" + i + ". Result = " + result);
+        }
+
+        System.out.println("Result = " + result);
+        return result;
+    }
+
+    public static int handleFrameStrike(int idx, String[] frames) {
+        int result = 10;
+
+        int calculatedRolls = 0;
+
+        int nextIdx = idx;
+        while (calculatedRolls < 2) {
+            nextIdx++;
+            char[] nextCharArray = frames[nextIdx].toCharArray();
+
+            for (int i = 0; i < nextCharArray.length; i++) {
+                if (calculatedRolls >= 2) break;
+                result += getRollRawScore(i == 0 ? '0' : nextCharArray[i - 1], nextCharArray[i]);
+                calculatedRolls++;
+                System.out.println("Calculated rolls = " + calculatedRolls + ". Result = " + result);
+            }
+        }
+
+        System.out.println("Result = " + result);
+        return result;
+    }
+
+    public static int handleFrameSpare(int idx, String[] frames) {
+        int result = 10;
+
+        int nextIdx = idx + 1;
+        char[] nextCharArray = frames[nextIdx].toCharArray();
+
+        result += getRollRawScore('0', nextCharArray[0]);
+
+        System.out.println("Result = " + result);
+        return result;
+    }
+
+
+    public static int getRollRawScore(char prevChar, char currChar) {
+        if (currChar == 'X') return 10;
+        if (currChar == '/') return 10 - Character.getNumericValue(prevChar);
+        return Character.getNumericValue(currChar);
+
     }
 
     public static int handleFrameOrdinary(String frame) {
@@ -49,6 +107,8 @@ public class Solution {
         for (char c : chars) {
             result += Character.getNumericValue(c);
         }
+
+        System.out.println("Result = " + result);
         return result;
 
     }
--- src/test/java/idv/kuma/_4kyu/ten_pin_bowling/SolutionTest.java
+++ src/test/java/idv/kuma/_4kyu/ten_pin_bowling/SolutionTest.java
@@ -22,6 +22,27 @@ public class SolutionTest {
 
     }
 
+
+    @Test
+    public void basic_strike() throws Exception {
+        assertEquals(28, Solution.bowling_score("X 54 00 00 00 00 00 00 00 00"));
+        assertEquals(9 + 19 + (10 + 10 + 5), Solution.bowling_score("X X 54 00 00 00 00 00 00 00"));
+        assertEquals(9 + 15 + 20 + (10 + 10 + 5), Solution.bowling_score("X X 5/ 54 00 00 00 00 00 00"));
+
+    }
+
+    @Test
+    public void last_frame() throws Exception {
+        assertEquals(30, Solution.bowling_score("00 00 00 00 00 00 00 00 00 XXX"));
+
+    }
+
+    @Test
+    public void basic_spare() throws Exception {
+        assertEquals((10 + 5) + 9, Solution.bowling_score("5/ 54 00 00 00 00 00 00 00 00"));
+
+    }
+
     @Test
     public void all_ones() throws Exception {
         // assertEquals("expected", "actual");
@@ -29,7 +50,6 @@ public class SolutionTest {
         assertEquals(20, Solution.bowling_score("11 11 11 11 11 11 11 11 11 11"));
     }
 
-    @Ignore
     @Test
     public void perfect_game() {
         System.out.println("Woah! Perfect game!");
