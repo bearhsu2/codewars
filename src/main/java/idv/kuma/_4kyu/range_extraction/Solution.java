ee78d5e0b928082990a359460454ea849e7de4df
KUMA-PC\bearhsu2 <bearhsu2@gmail.com>
2018-04-15 23:15:07 +0800

    re-factor

--- src/main/java/idv/kuma/_4kyu/range_extraction/Solution.java
+++ src/main/java/idv/kuma/_4kyu/range_extraction/Solution.java
@@ -1,11 +1,16 @@
 package idv.kuma._4kyu.range_extraction;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public static String rangeExtraction(int[] arr) {
+        return printFriendly(arr);
+    }
 
-        return "1,3,5,7,9";
-
+    private static String printFriendly(int[] arrIn) {
+        return Arrays.stream(arrIn)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
     }
 }
