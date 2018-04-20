35cfa327dcdcafd11e6db629238d007a9b042302
Yu-Song Syu <kuma@simbo.com.tw>
2018-04-20 23:53:42 +0800

    4kyu: machine learning game

--- src/main/java/idv/kuma/_4kyu/machine_learning_game/MachineLearning.java
+++ src/main/java/idv/kuma/_4kyu/machine_learning_game/MachineLearning.java
@@ -1,73 +1,46 @@
 package idv.kuma._4kyu.machine_learning_game;
 
-import static idv.kuma._4kyu.machine_learning_game.MachineLearning.Actions.FUNCTIONS;
-
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 
 public class MachineLearning {
 
     int currentCmd;
-    int suggestedAction;
     int trainedCounts;
 
-    List<Pair> candidatePairs;
-    Map<Integer, Integer> correctPairs;
+    Map<Integer, Integer> correctMappings;
 
     public MachineLearning() {
 
-        // tip: cmd can be very large number
-        candidatePairs = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                candidatePairs.add(new Pair(i, j));
-            }
-        }
-        correctPairs = new HashMap<>();
+        correctMappings = new HashMap<>();
         trainedCounts = 0;
     }
 
     public int command(int cmd, int num) {
         currentCmd = cmd;
-        if (trainedCounts >= 20) {
 
-            if (correctPairs.size() < 5){
-                for (Pair pair : candidatePairs){
-                    if (!correctPairs.containsKey(pair.getKey()) && !correctPairs.containsValue(pair.getValue())){
-                        correctPairs.put(pair.getKey(), pair.getValue());
-                    }
-                }
-            }
+        if (!correctMappings.containsKey(cmd)) correctMappings.put(cmd, 0);
 
-            suggestedAction = correctPairs.get(cmd);
-        } else {
-            suggestedAction = candidatePairs
-                    .stream()
-                    .filter(pair -> pair.getKey() == cmd)
-                    .collect(Collectors.toList())
-                    .get(0)
-                    .getValue();
-        }
+        int suggestedAction = correctMappings.get(cmd);
 
         System.out.println("train " + trainedCounts + ", num = " + num + ", cmd = " + cmd + ", action = " + suggestedAction);
 
         trainedCounts++;
-        return FUNCTIONS.get(suggestedAction).apply(num);
+        return Actions.FUNCTIONS.get(suggestedAction).apply(num);
     }
 
     public void response(boolean result) {
+
+        int suggestedAction = correctMappings.get(currentCmd);
+
         if (result) {
-            System.out.println("Correct answer => add pair (" + currentCmd + ", " + suggestedAction + ") to correct pairs");
-            correctPairs.put(currentCmd, suggestedAction);
+            System.out.println("Correct answer => pair (" + currentCmd + ", " + suggestedAction + ") remains in correct mappings");
         } else {
-            // Remove (cmd, action) pair from candidate pairs
-            System.out.println("Wrong answer => remove pair (" + currentCmd + ", " + suggestedAction + ") from candidate pairs");
-            candidatePairs = candidatePairs
-                    .stream()
-                    .filter(pair -> !(pair.getKey() == currentCmd && pair.getValue() == suggestedAction))
-                    .collect(Collectors.toList());
+            // Suggest another answer candidate next time
+            suggestedAction++;
+            System.out.println("Wrong answer => suggest pair (" + currentCmd + ", " + suggestedAction + ") next time");
+            correctMappings.put(currentCmd, suggestedAction);
         }
     }
 
@@ -75,32 +48,5 @@ public class MachineLearning {
         static final List<Function<Integer, Integer>> FUNCTIONS = Arrays.asList(x -> x + 1, x -> 0, x -> x % 2, x -> x / 2, x -> x * 100);
     }
 
-    static class Pair{
-        int key;
-        int value;
-
-
-        public Pair(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public int getKey() {
-            return key;
-        }
-
-        public void setKey(int key) {
-            this.key = key;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-
-    }
 
 }
