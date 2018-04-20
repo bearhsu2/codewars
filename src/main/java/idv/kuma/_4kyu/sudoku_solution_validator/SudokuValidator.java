ce4bb991baa323bffa691bf799e4f2dc0427aca8
Yu-Song Syu <kuma@simbo.com.tw>
2018-04-20 23:55:18 +0800

    init

---
+++ src/main/java/idv/kuma/_4kyu/sudoku_solution_validator/SudokuValidator.java
@@ -0,0 +1,9 @@
+package idv.kuma._4kyu.sudoku_solution_validator;
+
+public class SudokuValidator {
+    public static boolean check(int[][] sudoku) {
+        //do your magic
+
+        return true;
+    }
+}
