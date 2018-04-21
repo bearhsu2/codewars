6f4f67977e8a737e0f0b8f405e73ca914f6d4161
Yu-Song Syu <kuma@simbo.com.tw>
2018-04-21 23:04:18 +0800

    ok

--- src/main/java/idv/kuma/_4kyu/sudoku_solution_validator/SudokuValidator.java
+++ src/main/java/idv/kuma/_4kyu/sudoku_solution_validator/SudokuValidator.java
@@ -1,7 +1,9 @@
 package idv.kuma._4kyu.sudoku_solution_validator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class SudokuValidator {
     public static boolean check(int[][] sudoku) {
@@ -33,6 +35,30 @@ public class SudokuValidator {
     }
 
     private static boolean checkSubBlocks(int[][] sudoku){
+
+
+        int length = sudoku.length;
+        List<Integer>[][] subArrays = new ArrayList[3][3];
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < length; j++){
+                int indexI = i/3;
+                int indexJ = j/3;
+                if (subArrays[indexI][indexJ] == null){
+                    subArrays[indexI][indexJ] = new ArrayList<>();
+                }
+                subArrays[i/3][j/3].add(sudoku[i][j]);
+            }
+        }
+
+        for (int i = 0; i < subArrays.length; i++){
+            for (int j = 0; j < subArrays[0].length; j++){
+                List<Integer> list = subArrays[i][j];
+
+                if (!list.stream().allMatch(new HashSet<>()::add) || list.stream().anyMatch(e -> e == 0)) return false;
+            }
+        }
+
+
         return true;
     }
 }
