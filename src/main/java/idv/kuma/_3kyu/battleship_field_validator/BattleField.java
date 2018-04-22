5a7bdde313c95dd3fed56d2ffbd9fd5e619226e0
Yu-Song Syu <kuma@simbo.com.tw>
2018-04-22 14:17:52 +0800

    temp

--- src/main/java/idv/kuma/_3kyu/battleship_field_validator/BattleField.java
+++ src/main/java/idv/kuma/_3kyu/battleship_field_validator/BattleField.java
@@ -1,9 +1,35 @@
 package idv.kuma._3kyu.battleship_field_validator;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class BattleField {
 
+
+//    https://stackoverflow.com/questions/7501344/how-to-validate-battleship-field
+
     public static boolean fieldValidator(int[][] field) {
-        // your code here!
+
+        if (!preCheck(field)) return false;
+
+        occupied = new int[field.length][field[0].length];
+
+        doProcess(field);
+
+
         return false;
     }
+
+    private  static boolean preCheck(int[][] field){
+        return true;
+    }
+
+    private static void doProcess(int[][] field){
+        for (){
+
+        }
+    }
+
 }
