45710f2759ceb47cf7038bf6abb4f7c058e409bb
Yu-Song Syu <kuma@simbo.com.tw>
2018-04-22 11:58:48 +0800

    init

---
+++ src/main/java/idv/kuma/_3kyu/battleship_field_validator/BattleField.java
@@ -0,0 +1,9 @@
+package idv.kuma._3kyu.battleship_field_validator;
+
+public class BattleField {
+
+    public static boolean fieldValidator(int[][] field) {
+        // your code here!
+        return false;
+    }
+}
---
+++ src/test/java/idv/kuma/_3kyu/battleship_field_validator/BattleFieldTest.java
@@ -0,0 +1,26 @@
+package idv.kuma._3kyu.battleship_field_validator;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BattleFieldTest {
+
+    private static int[][] battleField =
+            {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    @Test
+    public void SampleTest() {
+        assertEquals(true, BattleField.fieldValidator(battleField));
+    }
+}
