8c9ca82e4f7222558f56897ea92d04a4c443ae17
Yu-Song Syu <kuma@simbo.com.tw>
2018-06-10 00:46:39 +0800

    0 ok

---
+++ src/main/java/idv/kuma/_4kyu/binary_multiple_of_3/BinaryRegexp.java
@@ -0,0 +1,12 @@
+package idv.kuma._4kyu.binary_multiple_of_3;
+
+import java.util.regex.Pattern;
+
+public class BinaryRegexp {
+
+    public static Pattern multipleOf3() {
+        // Regular expression that matches binary inputs that are multiple of 3
+        return Pattern.compile("0");
+    }
+
+}
---
+++ src/test/java/idv/kuma/_4kyu/binary_multiple_of_3/BinaryRegexpTest.java
@@ -0,0 +1,27 @@
+package idv.kuma._4kyu.binary_multiple_of_3;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinaryRegexpTest {
+
+    @Test
+    public void Test_0_true() throws Exception {
+        assertEquals(true, BinaryRegexp.multipleOf3().matcher("0").matches());
+    }
+
+    @Ignore
+    @Test
+    public void testSimple() {
+        assertEquals(false, BinaryRegexp.multipleOf3().matcher(" 0").matches());
+        assertEquals(false, BinaryRegexp.multipleOf3().matcher("abc").matches());
+        assertEquals(true, BinaryRegexp.multipleOf3().matcher("000").matches());
+
+        assertEquals(true, BinaryRegexp.multipleOf3().matcher("110").matches());
+        assertEquals(false, BinaryRegexp.multipleOf3().matcher("111").matches());
+        assertEquals(true, BinaryRegexp.multipleOf3().matcher(Integer.toBinaryString(12345678)).matches());
+    }
+
+}
