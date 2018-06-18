ed6bd2ecbf746238e6e3d2b27cf50ef54f681c1f
Yu-Song Syu <kuma@simbo.com.tw>
2018-06-18 21:09:00 +0800

    60 <= seconds OK

--- src/main/java/idv/kuma/_4kyu/time_formatter/TimeFormatter.java
+++ src/main/java/idv/kuma/_4kyu/time_formatter/TimeFormatter.java
@@ -2,21 +2,30 @@ package idv.kuma._4kyu.time_formatter;
 
 public class TimeFormatter {
 
+    private static StringBuilder sb;
+
     public static String formatDuration(int seconds) {
         // your code goes here
 
         if (0 == seconds) return "now";
 
 
-        if (60 == seconds){
-            return "1 minute";
-        } if (61 == seconds){
-            return "1 minute and 1 second";
+        sb = new StringBuilder();
+
+        if (60 <= seconds) {
+            if (60 == seconds) {
+                sb.append("1 minute");
+            } else if (61 == seconds) {
+                sb.append("1 minute and 1 second");
+            }
+
+            return sb.toString();
+
+        } else {
+
+            String result = makeSecondString(seconds);
+            return result;
         }
-
-
-        String result = makeSecondString(seconds);
-        return result;
 
     }
 
