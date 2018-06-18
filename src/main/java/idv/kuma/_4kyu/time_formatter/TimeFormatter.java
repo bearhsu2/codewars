c769d4ffc174452a4e28af976ff6baf1d674a9b8
Yu-Song Syu <kuma@simbo.com.tw>
2018-06-18 21:16:33 +0800

    find common rules

--- src/main/java/idv/kuma/_4kyu/time_formatter/TimeFormatter.java
+++ src/main/java/idv/kuma/_4kyu/time_formatter/TimeFormatter.java
@@ -13,12 +13,23 @@ public class TimeFormatter {
         sb = new StringBuilder();
 
         if (60 <= seconds) {
-            if (60 == seconds) {
-                sb.append("1 minute");
-            } else if (61 == seconds) {
-                sb.append("1 minute and 1 second");
-            }
 
+            int minutes = 1;
+
+            sb.append(minutes);
+            sb.append(" ");
+            sb.append("minute");
+            sb.append(((minutes == 1) ? "" : "s"));
+
+            if (61 <= seconds){
+                seconds -= 60;
+                sb.append(" and ");
+
+                sb.append(seconds);
+                sb.append(" ");
+                sb.append("second");
+                sb.append(((seconds == 1) ? "" : "s"));
+            }
         } else {
 
             sb.append(seconds);
