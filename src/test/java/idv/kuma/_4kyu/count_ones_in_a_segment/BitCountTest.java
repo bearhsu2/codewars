a546b9bd5e3d7c50f8f995d83d417ce59521af8f
Yu-Song Syu <kuma@simbo.com.tw>
2018-06-25 21:21:21 +0800

    refactor test

--- src/test/java/idv/kuma/_4kyu/count_ones_in_a_segment/BitCountTest.java
+++ src/test/java/idv/kuma/_4kyu/count_ones_in_a_segment/BitCountTest.java
@@ -11,23 +11,27 @@ public class BitCountTest {
 
     @Test
     public void Input_0_0_Should_Return_0() {
-        assertEquals(new BigInteger("0"), BitCount.countOnes(0,0));
+        runAndCheck("0", 0, 0);
     }
 
     @Test
     public void Input_1_1_Should_Return_1() {
-        assertEquals(new BigInteger("1"), BitCount.countOnes(1,1));
+        runAndCheck("1", 1, 1);
     }
 
     @Ignore
     @Test
     public void Input_5_7_Should_Return_7() {
-        assertEquals(new BigInteger("7"), BitCount.countOnes(5,7));
+        runAndCheck("7", 5, 7);
     }
 
     @Ignore
     @Test
     public void Input_12_29_Should_Return_51() {
-        assertEquals(new BigInteger("51"), BitCount.countOnes(12,29));
+        runAndCheck("51", 12, 29);
+    }
+
+    void runAndCheck(String expected, int left, int right) {
+        assertEquals(new BigInteger(expected), BitCount.countOnes(left, right));
     }
 }
