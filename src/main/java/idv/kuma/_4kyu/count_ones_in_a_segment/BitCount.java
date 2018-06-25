052451f3bdb844da423f81dedbca05228faa3041
Yu-Song Syu <kuma@simbo.com.tw>
2018-06-25 21:04:52 +0800

    init version

---
+++ src/main/java/idv/kuma/_4kyu/count_ones_in_a_segment/BitCount.java
@@ -0,0 +1,11 @@
+package idv.kuma._4kyu.count_ones_in_a_segment;
+
+import java.math.BigInteger;
+
+public class BitCount {
+
+    public static BigInteger countOnes(long left, long right) {
+        // Your code here
+        return null;
+    }
+}
