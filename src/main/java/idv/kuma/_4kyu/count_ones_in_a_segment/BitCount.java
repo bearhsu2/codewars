700e7f22b70faa540c02a3d5d78f4733511304fa
Yu-Song Syu <kuma@simbo.com.tw>
2018-06-27 00:12:01 +0800

    remove unused codes

--- src/main/java/idv/kuma/_4kyu/count_ones_in_a_segment/BitCount.java
+++ src/main/java/idv/kuma/_4kyu/count_ones_in_a_segment/BitCount.java
@@ -1,44 +1,12 @@
 package idv.kuma._4kyu.count_ones_in_a_segment;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 
 public class BitCount {
 
-
-    public static Map<Long, BigInteger> onesForSingle = new HashMap<>();
-
     public static BigInteger countOnes(long left, long right) {
-
         return countSumOfOnesForSingle(right).subtract(countSumOfOnesForSingle(left - 1));
-
-//        BigInteger result = BigInteger.valueOf(0L);
-//        for (long i = left; i <= right; i++) {
-//            result = result.add(findOnesForSingle(i));
-//        }
-//
-//        return result;
-
     }
-
-    private static BigInteger findOnesForSingle(long number) {
-        BigInteger result = onesForSingle.get(number);
-
-        if (result == null){
-            result = countOnesForSingle(number);
-            onesForSingle.put(number, result);
-        }
-
-        return  result;
-
-    }
-
-
-    static BigInteger countOnesForSingle(long number) {
-        return BigInteger.valueOf(Long.toBinaryString(number).replaceAll("0", "").length());
-    }
-
 
     static BigInteger countSumOfOnesOnOneDigitForSingle(long number, int column){
 
@@ -46,12 +14,9 @@ public class BitCount {
 
         long quotient = (number + 1) / divider;
 
-        long temp = quotient * divider;
-
-        long rest = (number + 1) - temp;
+        long rest = (number + 1) - quotient * divider;
 
         return BigInteger.valueOf(quotient * (long) Math.pow(2, column - 1)+ Math.max(0, rest - (long) Math.pow(2, column - 1)));
-
     }
 
     static BigInteger countSumOfOnesForSingle(long number){
@@ -60,7 +25,7 @@ public class BitCount {
         for (int column = 1; column <= 48; column++){
             result = result.add(countSumOfOnesOnOneDigitForSingle(number, column));
         }
-
+        
         return result;
     }
 }
