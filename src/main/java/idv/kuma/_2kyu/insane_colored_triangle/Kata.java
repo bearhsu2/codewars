950e13e4136cbb652d057e57697e783e65163aad
Yu-Song Syu <kuma@simbo.com.tw>
2018-07-05 23:09:17 +0800

    two lines refactor

--- src/main/java/idv/kuma/_2kyu/insane_colored_triangle/Kata.java
+++ src/main/java/idv/kuma/_2kyu/insane_colored_triangle/Kata.java
@@ -9,36 +9,38 @@ public class Kata {
             return row.charAt(0);
         }
 
-
+        
         char[] charArray = row.toCharArray();
-        if (charArray[0] == charArray[1]){
+        return findNextForTwoChars(charArray);
+
+
+    }
+
+    static char findNextForTwoChars(char[] charArray) {
+
+        if (charArray[0] == charArray[1]) {
             return charArray[0];
-        } else {
-
-            if (charArray[0] == 'R' && charArray[1] == 'G'){
-                return 'B';
-            }
-            if (charArray[0] == 'G' && charArray[1] == 'R'){
-                return 'B';
-            }
-            if (charArray[0] == 'G' && charArray[1] == 'B'){
-                return 'R';
-            }
-            if (charArray[0] == 'B' && charArray[1] == 'G'){
-                return 'R';
-            }
-            if (charArray[0] == 'R' && charArray[1] == 'B'){
-                return 'G';
-            }
-            if (charArray[0] == 'B' && charArray[1] == 'R'){
-                return 'G';
-            }
-
-
-
-            return 'B';
         }
 
+
+        if (charArray[0] == 'R' && charArray[1] == 'G') {
+            return 'B';
+        }
+        if (charArray[0] == 'G' && charArray[1] == 'R') {
+            return 'B';
+        }
+        if (charArray[0] == 'G' && charArray[1] == 'B') {
+            return 'R';
+        }
+        if (charArray[0] == 'B' && charArray[1] == 'G') {
+            return 'R';
+        }
+        if (charArray[0] == 'R' && charArray[1] == 'B') {
+            return 'G';
+        }
+
+        // (charArray[0] == 'B' && charArray[1] == 'R') {
+        return 'G';
 
 
     }
