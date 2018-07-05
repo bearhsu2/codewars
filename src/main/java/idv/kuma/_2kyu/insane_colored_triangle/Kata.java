927da5c9483a5a5d68a3263d22c2186cda84f3d1
Yu-Song Syu <kuma@simbo.com.tw>
2018-07-05 22:53:20 +0800

    refactor

--- src/main/java/idv/kuma/_2kyu/insane_colored_triangle/Kata.java
+++ src/main/java/idv/kuma/_2kyu/insane_colored_triangle/Kata.java
@@ -5,13 +5,7 @@ public class Kata {
     // https://www.codewars.com/kata/5a331ea7ee1aae8f24000175/train/java
     public static char triangle(final String row) {
 
-        if (row.equals("B")) {
-            return 'B';
-        } else if (row.equals("G")) {
-            return 'G';
-        }
-
-        return 'R';
+        return row.charAt(0);
     }
 
 
