e5b7381a105bb0fb1875e1c1a28b59a9808d469b
Yu-Song Syu <kuma@simbo.com.tw>
2018-07-06 21:41:09 +0800

    no need if. a map fits lenth-1 and length-2

--- src/main/java/idv/kuma/_7kyu/coloured_triangles/Kata.java
+++ src/main/java/idv/kuma/_7kyu/coloured_triangles/Kata.java
@@ -27,10 +27,7 @@ public class Kata {
     // https://www.codewars.com/kata/5a331ea7ee1aae8f24000175/train/java
     public static char triangle(final String row) {
 
-        if (row.length() == 1) {
-            return row.charAt(0);
-        }
-
+       
 
         return findNextForTwoCharsString(row);
 
