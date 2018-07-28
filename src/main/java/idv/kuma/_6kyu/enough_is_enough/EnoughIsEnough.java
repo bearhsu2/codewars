d569eb84f8b51ab46f641cec3c1d63ea3fd5c0fb
Yu-Song Syu <kuma@simbo.com.tw>
2018-07-28 23:51:30 +0800

    add counting functions

--- src/main/java/idv/kuma/_6kyu/enough_is_enough/EnoughIsEnough.java
+++ src/main/java/idv/kuma/_6kyu/enough_is_enough/EnoughIsEnough.java
@@ -1,14 +1,40 @@
 package idv.kuma._6kyu.enough_is_enough;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EnoughIsEnough {
 
+    private static Map<Integer, Integer> occurrences = new HashMap<>();
+
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
-      
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < elements.length; i++) {
+
+            int element = elements[i];
+
+            Integer occurrence = occurrences.get(element);
+
+            if (null == occurrence){
+                occurrence = 1;
+                occurrences.put(element, 1);
+            } else {
+                occurrence += 1;
+                occurrences.put(element, occurrence);
+            }
+
+            if (occurrence <= maxOccurrences){
+                result.add(element);
+            }
+
+        }
 
 
-
-
-        return elements;
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
 }
