d8c2a0b81ecbd0024185829a8d1cf5be5bb1b4ba
Yu-Song Syu <kuma@simbo.com.tw>
2018-07-28 23:37:50 +0800

    all one time OK

---
+++ src/main/java/idv/kuma/_6kyu/enough_is_enough/EnoughIsEnough.java
@@ -0,0 +1,14 @@
+package idv.kuma._6kyu.enough_is_enough;
+
+public class EnoughIsEnough {
+
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+      
+
+
+
+
+        return elements;
+    }
+
+}
