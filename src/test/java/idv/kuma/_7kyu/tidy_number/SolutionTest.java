ae7d6a7eae24b3db37407188601c99d9c5033c45
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-12 22:22:28 +0800

    refactor test

--- src/test/java/idv/kuma/_7kyu/tidy_number/SolutionTest.java
+++ src/test/java/idv/kuma/_7kyu/tidy_number/SolutionTest.java
@@ -9,30 +9,34 @@ public class SolutionTest {
 
     @Test
     public void Check_1_true() throws Exception {
-        assertEquals(true, Solution.tidyNumber(1));
+        runAndCheck(true, Solution.tidyNumber(1));
     }
 
-
+    
     @Test
     public void Check_10_false() throws Exception {
-        assertEquals(false, Solution.tidyNumber(10));
+        runAndCheck(false, Solution.tidyNumber(10));
     }
 
 
     @Ignore
     @Test
     public void Check_Small_Values() {
-        assertEquals(true, Solution.tidyNumber(12));
-        assertEquals(false, Solution.tidyNumber(32));
-        assertEquals(true, Solution.tidyNumber(39));
+        runAndCheck(true, Solution.tidyNumber(12));
+        runAndCheck(false, Solution.tidyNumber(32));
+        runAndCheck(true, Solution.tidyNumber(39));
     }
 
     @Ignore
     @Test
     public void Check_Larger_Values() {
-        assertEquals(false, Solution.tidyNumber(1024));
-        assertEquals(false, Solution.tidyNumber(12576));
-        assertEquals(true, Solution.tidyNumber(13579));
-        assertEquals(true, Solution.tidyNumber(2335));
+        runAndCheck(false, Solution.tidyNumber(1024));
+        runAndCheck(false, Solution.tidyNumber(12576));
+        runAndCheck(true, Solution.tidyNumber(13579));
+        runAndCheck(true, Solution.tidyNumber(2335));
+    }
+
+    void runAndCheck(boolean expected, boolean actual) {
+        assertEquals(expected, actual);
     }
 }
