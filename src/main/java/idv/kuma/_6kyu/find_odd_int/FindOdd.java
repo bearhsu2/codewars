f2a1f7889c344a5d89c1c0b81aa46996c860729a
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-13 22:09:11 +0800

    tests ok

--- src/main/java/idv/kuma/_6kyu/find_odd_int/FindOdd.java
+++ src/main/java/idv/kuma/_6kyu/find_odd_int/FindOdd.java
@@ -1,7 +1,27 @@
 package idv.kuma._6kyu.find_odd_int;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindOdd {
+
     public static int findIt(int[] a) {
-        return 10;
+
+        Map<Integer, Integer> timesRecord = new HashMap<>();
+
+        for (int i = 0; i < a.length; i++) {
+            int key = a[i];
+
+            Integer times = timesRecord.get(key);
+
+            if (null == times) {
+                timesRecord.put(key, 1);
+            } else {
+                timesRecord.put(key, times + 1);
+            }
+        }
+
+        return timesRecord.entrySet().stream().filter(entry -> entry.getValue().intValue() % 2 == 1).findFirst().get().getKey();
+
     }
 }
