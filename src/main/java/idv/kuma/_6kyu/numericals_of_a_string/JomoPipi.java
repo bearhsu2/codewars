39bebeaedfbe10a591d32885d86cc2d41df8529d
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-29 00:23:03 +0800

    abc 111

---
+++ src/main/java/idv/kuma/_6kyu/numericals_of_a_string/JomoPipi.java
@@ -0,0 +1,7 @@
+package idv.kuma._6kyu.numericals_of_a_string;
+
+public class JomoPipi {
+    public static String numericals(String s) {
+        return "111";
+    }
+}
---
+++ src/test/java/idv/kuma/_6kyu/numericals_of_a_string/SolutionTest.java
@@ -0,0 +1,38 @@
+package idv.kuma._6kyu.numericals_of_a_string;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+
+
+    @Test
+    public void abc_111() throws Exception {
+        assertEquals("111", JomoPipi.numericals("abc"));
+    }
+
+
+    @Ignore
+    @Test
+    public void basicTest1() {
+        assertEquals("1112111121311", JomoPipi.numericals("Hello, World!"));
+    }
+
+
+    @Ignore
+    @Test
+    public void basicTest2() {
+        assertEquals("11121111213112111131224132411122", JomoPipi.numericals("Hello, World! It's me, JomoPipi!"));
+    }
+
+    @Ignore
+    @Test
+    public void basicTest3() {
+        assertEquals("11121122342", JomoPipi.numericals("hello hello"));
+    }
+
+}
