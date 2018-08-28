dffded31113fa0333b6c46a5b5104d83a4efe234
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-29 00:32:28 +0800

    re-construct

--- src/main/java/idv/kuma/_6kyu/numericals_of_a_string/JomoPipi.java
+++ src/main/java/idv/kuma/_6kyu/numericals_of_a_string/JomoPipi.java
@@ -1,13 +1,42 @@
 package idv.kuma._6kyu.numericals_of_a_string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JomoPipi {
 
     public static String numericals(String s) {
 
-        if (s.equals("abc")){
-            return "111";
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        char[] charArray = s.toCharArray();
+
+
+        for (char c : charArray) {
+
+            Integer count = countMap.get(c);
+
+            if (count == null) {
+                count = 1;
+                countMap.put(c, count);
+            } else {
+
+                count++;
+                countMap.put(c, count);
+            }
+
+            sb.append(count);
         }
 
-        return "1112";
+        return sb.toString();
+
+//
+//        if (s.equals("abc")) {
+//            return "111";
+//        }
+//
+//        return "1112";
     }
 }
