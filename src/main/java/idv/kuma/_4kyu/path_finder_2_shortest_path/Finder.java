0e386264a9643c0532611cc7363884c1b0180f24
Yu-Song Syu <kuma@simbo.com.tw>
2018-09-16 20:34:35 +0800

    initial

---
+++ src/main/java/idv/kuma/_4kyu/path_finder_2_shortest_path/Finder.java
@@ -0,0 +1,8 @@
+package idv.kuma._4kyu.path_finder_2_shortest_path;
+
+public class Finder {
+
+    public static int pathFinder(String maze) {
+        return 0;
+    }
+}
---
+++ src/test/java/idv/kuma/_4kyu/path_finder_2_shortest_path/FinderTest.java
@@ -0,0 +1,7 @@
+package idv.kuma._4kyu.path_finder_2_shortest_path;
+
+import static org.junit.Assert.*;
+
+public class FinderTest {
+
+}
