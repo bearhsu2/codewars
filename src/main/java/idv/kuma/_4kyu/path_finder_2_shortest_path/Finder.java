4def1335da395df639b25ff0d5d03b28d166d599
Yu-Song Syu <kuma@simbo.com.tw>
2018-09-16 21:59:52 +0800

    temp commit

--- src/main/java/idv/kuma/_4kyu/path_finder_2_shortest_path/Finder.java
+++ src/main/java/idv/kuma/_4kyu/path_finder_2_shortest_path/Finder.java
@@ -1,6 +1,8 @@
 package idv.kuma._4kyu.path_finder_2_shortest_path;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Finder {
@@ -18,9 +20,51 @@ public class Finder {
 
         Map<String, Integer> distances = makeDistanceMap(lines);
         Map<String, Boolean> calculated = makeCalculatedMap(distances);
+        List<String> waitingForCalculation = makeWaitingList(distances);
+        String currentKey = "0_0";
+
+        while (!isFinished(calculated, n)) {
+
+            // add waitingList candidates
+            for (findUncalculatedNeighbors(currentKey, calculated)) {
+
+            }
+
+        }
 
 
         return 0;
+    }
+
+    private static void findUncalculatedNeighbors(String currentKey, Map<String, Boolean> calculated) {
+        int i = findI(currentKey);
+        int j = findJ(currentKey);
+
+        for (){
+
+        }
+        
+
+    }
+
+    private static int findI(String currentKey) {
+        return Integer.valueOf(currentKey.split("_")[0]);
+    }
+
+    private static int findJ(String currentKey) {
+        return Integer.valueOf(currentKey.split("_")[1]);
+    }
+
+    private static List<String> makeWaitingList(Map<String, Integer> distances) {
+        List<String> result = new ArrayList<>();
+
+        result.add("0_0");
+
+        return result;
+    }
+
+    private static boolean isFinished(Map<String, Boolean> calculated, int n) {
+        return calculated.get(makeKey(n - 1, n - 1));
     }
 
 
