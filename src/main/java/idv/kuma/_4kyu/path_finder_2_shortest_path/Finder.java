6fe4c666098046fff0cd0871f9a61ecc036eda77
Yu-Song Syu <kuma@simbo.com.tw>
2018-09-16 21:47:01 +0800

    1 x 1 OK

--- src/main/java/idv/kuma/_4kyu/path_finder_2_shortest_path/Finder.java
+++ src/main/java/idv/kuma/_4kyu/path_finder_2_shortest_path/Finder.java
@@ -1,8 +1,65 @@
 package idv.kuma._4kyu.path_finder_2_shortest_path;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Finder {
 
     public static int pathFinder(String maze) {
+
+        String[] lines = maze.split("\\r?\\n");
+        int n = lines.length;
+        System.out.println("n = " + n);
+
+
+        if (lines[n - 1].charAt(n - 1) == 'W') {
+            return -1;
+        }
+
+        Map<String, Integer> distances = makeDistanceMap(lines);
+        Map<String, Boolean> calculated = makeCalculatedMap(distances);
+
+
         return 0;
     }
+
+
+    private static Map<String, Integer> makeDistanceMap(String[] lines) {
+
+        Map<String, Integer> result = new HashMap<>();
+        for (int i = 0; i < lines.length; i++) {
+            char[] chars = lines[i].toCharArray();
+
+            for (int j = 0; j < chars.length; j++) {
+                if (isRoad(chars[j])) {
+                    result.put(makeKey(i, j), Integer.MAX_VALUE);
+                }
+            }
+
+        }
+
+
+        return result;
+    }
+
+    private static Map<String, Boolean> makeCalculatedMap(Map<String, Integer> distances) {
+        Map<String, Boolean> calculated = new HashMap<>();
+
+        distances.keySet().forEach(name -> {
+            calculated.put(name, Boolean.FALSE);
+        });
+
+        return calculated;
+    }
+
+    private static boolean isRoad(int j) {
+        return j == '.';
+    }
+
+
+    private static String makeKey(int i, int j) {
+        return i + "_" + j;
+    }
+
+
 }
--- src/test/java/idv/kuma/_4kyu/path_finder_2_shortest_path/FinderTest.java
+++ src/test/java/idv/kuma/_4kyu/path_finder_2_shortest_path/FinderTest.java
@@ -1,38 +1,63 @@
 package idv.kuma._4kyu.path_finder_2_shortest_path;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FinderTest {
 
-    @Test public void sampleTests() {
+    @Test
+    public void _1_x_1_No_Wall_Then_Distance_Is_1() throws Exception {
+        String a = ".";
+        assertEquals(a, 0, Finder.pathFinder(a));
+    }
 
-        String a = ".W.\n"+
-                ".W.\n"+
+
+    @Test
+    public void _1_x_1_Wall_Then_Distance_Is_Negative_1() throws Exception {
+        String a = "W";
+        assertEquals(a, -1, Finder.pathFinder(a));
+    }
+
+    @Ignore
+    @Test
+    public void _3_x_3_No_Wall_Then_Distance_Is_4() throws Exception {
+        String a = "...\n" +
+                "...\n" +
+                "...";
+        assertEquals(a, 4, Finder.pathFinder(a));
+    }
+
+    @Ignore
+    @Test
+    public void sampleTests() {
+
+        String a = ".W.\n" +
+                ".W.\n" +
                 "...",
 
-                b = ".W.\n"+
-                        ".W.\n"+
+                b = ".W.\n" +
+                        ".W.\n" +
                         "W..",
 
-                c = "......\n"+
-                        "......\n"+
-                        "......\n"+
-                        "......\n"+
-                        "......\n"+
+                c = "......\n" +
+                        "......\n" +
+                        "......\n" +
+                        "......\n" +
+                        "......\n" +
                         "......",
 
-                d = "......\n"+
-                        "......\n"+
-                        "......\n"+
-                        "......\n"+
-                        ".....W\n"+
+                d = "......\n" +
+                        "......\n" +
+                        "......\n" +
+                        "......\n" +
+                        ".....W\n" +
                         "....W.";
 
-        assertEquals(a, 4,  Finder.pathFinder(a));
+        assertEquals(a, 4, Finder.pathFinder(a));
         assertEquals(b, -1, Finder.pathFinder(b));
-        assertEquals(c, 10,  Finder.pathFinder(c));
+        assertEquals(c, 10, Finder.pathFinder(c));
         assertEquals(d, -1, Finder.pathFinder(d));
     }
 }
