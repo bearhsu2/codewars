25921983481691d9be6087fc3add14a959bb27b5
Yu-Song Syu <kuma@simbo.com.tw>
2018-09-16 20:35:28 +0800

    add test (fail)

--- src/test/java/idv/kuma/_4kyu/path_finder_2_shortest_path/FinderTest.java
+++ src/test/java/idv/kuma/_4kyu/path_finder_2_shortest_path/FinderTest.java
@@ -1,7 +1,39 @@
 package idv.kuma._4kyu.path_finder_2_shortest_path;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class FinderTest {
 
+    @Test public void sampleTests() {
+
+        String a = ".W.\n"+
+                ".W.\n"+
+                "...",
+
+                b = ".W.\n"+
+                        ".W.\n"+
+                        "W..",
+
+                c = "......\n"+
+                        "......\n"+
+                        "......\n"+
+                        "......\n"+
+                        "......\n"+
+                        "......",
+
+                d = "......\n"+
+                        "......\n"+
+                        "......\n"+
+                        "......\n"+
+                        ".....W\n"+
+                        "....W.";
+
+        assertEquals(a, 4,  Finder.pathFinder(a));
+        assertEquals(b, -1, Finder.pathFinder(b));
+        assertEquals(c, 10,  Finder.pathFinder(c));
+        assertEquals(d, -1, Finder.pathFinder(d));
+    }
 }
+
