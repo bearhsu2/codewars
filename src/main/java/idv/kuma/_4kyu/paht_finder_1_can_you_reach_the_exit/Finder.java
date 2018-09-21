9dc4ecc976c6de883d98ec9f870afaa557b3f430
Yu-Song Syu <kuma@simbo.com.tw>
2018-09-21 23:36:43 +0800

    initial

---
+++ src/main/java/idv/kuma/_4kyu/paht_finder_1_can_you_reach_the_exit/Finder.java
@@ -0,0 +1,9 @@
+package idv.kuma._4kyu.paht_finder_1_can_you_reach_the_exit;
+
+public class Finder {
+
+    static boolean pathFinder(String maze) {
+        // Your code here!!
+        return false;
+    }
+}
