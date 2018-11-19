533db61e2134b89a1d37cbc03eb6ac89096eaa18
Yu-Song Syu <kuma@simbo.com.tw>
2018-11-19 22:33:33 +0800

    official example tests ok

--- src/test/java/idv/kuma/_2kyu/integer_square_root/KataTest.java
+++ src/test/java/idv/kuma/_2kyu/integer_square_root/KataTest.java
@@ -18,16 +18,31 @@ public class KataTest {
         runAndCheck("4", "2");
     }
 
-
-    @Ignore
     @Test
-    public void basicTests() {
+    public void _3_1() throws Exception {
+        runAndCheck("3", "1");
+    }
+
+    @Test
+    public void _101_10() throws Exception {
+        runAndCheck("3", "1");
+    }
+
+
+    @Test
+    public void basicExampleTests() {
 
         runAndCheck("1", "1");
         runAndCheck("5", "2");
         runAndCheck("17", "4");
         runAndCheck("100", "10");
         runAndCheck("1000", "31");
+
+    }
+
+    @Ignore
+    @Test
+    public void binNums() throws Exception {
         runAndCheck("23232328323215435345345345343458098856756556809400840980980980980809092343243243243243098799634", "152421548093487868711992623730429930751178496967");
         runAndCheck("12323309809809534545458098709854808654685688665486860956865654654654324238000980980980", "3510457208086937291253621317073222057793129");
 
