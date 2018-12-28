d985eeb28869d0e5d85721a90ca8983274a27257
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-28 12:37:10 +0800

    refactor test: make mockedFrequencyMaker a  field

--- src/test/java/idv/kuma/_4kyu/strings_mix/MixingTest.java
+++ src/test/java/idv/kuma/_4kyu/strings_mix/MixingTest.java
@@ -13,6 +13,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static idv.kuma._4kyu.strings_mix.Mixing.*;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,10 +22,12 @@ import static org.mockito.Mockito.when;
 @PrepareForTest({Mixing.class})
 public class MixingTest {
 
+
+    private FrequencyMaker mockedFrequencyMaker= mock(FrequencyMaker.class);;
+
     @Test
     public void MockedTest_When_ccccddddd_aabbb_2ddddd_2cccc_1bbb_1aa() throws Exception {
 
-        Mixing.FrequencyMaker mockedFrequencyMaker = mock(Mixing.FrequencyMaker.class);
         when(mockedFrequencyMaker.makeStatics("ccccddddd", "1")).thenReturn(
                 new ArrayList<>(
                         Arrays.asList(
@@ -39,37 +42,37 @@ public class MixingTest {
 
                         )));
 
-        PowerMockito.whenNew(Mixing.FrequencyMaker.class).withNoArguments().thenReturn(mockedFrequencyMaker);
+        PowerMockito.whenNew(FrequencyMaker.class).withNoArguments().thenReturn(mockedFrequencyMaker);
 
 
-        Assert.assertEquals("1:ddddd/1:cccc/2:bbb/2:aa", Mixing.mix("ccccddddd", "aabbb"));
+        Assert.assertEquals("1:ddddd/1:cccc/2:bbb/2:aa", mix("ccccddddd", "aabbb"));
 
     }
 
     @Test
     public void When_aaa_bb_Then_1_aaa_2_bb() throws Exception {
-        assertEquals("1:aaa/2:bb", Mixing.mix("aaa", "bb"));
+        assertEquals("1:aaa/2:bb", mix("aaa", "bb"));
     }
 
     @Ignore
     @Test
     public void When_bb_aa_Then_1_aa_2_bb() throws Exception {
-        assertEquals("1:aa/2:bb", Mixing.mix("bb", "aa"));
+        assertEquals("1:aa/2:bb", mix("bb", "aa"));
     }
 
     @Ignore
     @Test
     public void test() {
         System.out.println("Mix Fixed Tests");
-        assertEquals("2:eeeee/2:yy/=:hh/=:rr", Mixing.mix("Are they here", "yes, they are here"));
+        assertEquals("2:eeeee/2:yy/=:hh/=:rr", mix("Are they here", "yes, they are here"));
         assertEquals("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg",
-                Mixing.mix("looping is fun but dangerous", "less dangerous than coding"));
+                mix("looping is fun but dangerous", "less dangerous than coding"));
         assertEquals("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt",
-                Mixing.mix(" In many languages", " there's a pair of functions"));
-        assertEquals("1:ee/1:ll/1:oo", Mixing.mix("Lords of the Fallen", "gamekult"));
-        assertEquals("", Mixing.mix("codewars", "codewars"));
+                mix(" In many languages", " there's a pair of functions"));
+        assertEquals("1:ee/1:ll/1:oo", mix("Lords of the Fallen", "gamekult"));
+        assertEquals("", mix("codewars", "codewars"));
         assertEquals("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr",
-                Mixing.mix("A generation must confront the looming ", "codewarrs"));
+                mix("A generation must confront the looming ", "codewarrs"));
     }
 
 }
