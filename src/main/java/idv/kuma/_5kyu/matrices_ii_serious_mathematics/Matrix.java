60cf1ed910c40c6f035810eaf4ded3ab390d8183
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-29 14:15:56 +0800

    2d constructor OK

--- src/main/java/idv/kuma/_5kyu/matrices_ii_serious_mathematics/Matrix.java
+++ src/main/java/idv/kuma/_5kyu/matrices_ii_serious_mathematics/Matrix.java
@@ -2,16 +2,47 @@ package idv.kuma._5kyu.matrices_ii_serious_mathematics;
 
 public class Matrix {
 
+    private double[][] elements;
+
     public Matrix(double[][] elements) {
-        // TODO: Write constructor
+
+        if (null == elements || elements.length == 0) throw new IllegalArgumentException();
+
+        int rowLength = -1;
+        this.elements = new double[elements.length][];
+
+        for (int i = 0; i < elements.length; i++) {
+            double[] row = elements[i];
+
+            int nextRowLength = row.length;
+            if (null == row
+                    || nextRowLength == 0
+                    || (nextRowLength != rowLength && rowLength >= 0)) throw new IllegalArgumentException();
+            rowLength = nextRowLength;
+
+            this.elements[i] = elements[i];
+        }
+
+        this.elements = elements;
     }
 
     public Matrix(int rows, int cols, double... elements) {
-        // TODO: Write constructor
+
+
+        this.elements = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                this.elements[i][j] = elements[i * cols + j];
+            }
+        }
+    }
+
+    public double getElement(int r, int c) {
+        return this.elements[r][c];
     }
 
     public double[][] toArray() {
-        return null;
+        return elements;
     }
 
     public Matrix transpose() {
