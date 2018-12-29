345584475178f4c5825e79210f2f59854e66b89d
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-29 10:24:00 +0800

    init

---
+++ src/main/java/idv/kuma/_5kyu/matrices_ii_serious_mathematics/Matrix.java
@@ -0,0 +1,36 @@
+package idv.kuma._5kyu.matrices_ii_serious_mathematics;
+
+public class Matrix {
+
+    public Matrix(double[][] elements) {
+        // TODO: Write constructor
+    }
+
+    public Matrix(int rows, int cols, double... elements) {
+        // TODO: Write constructor
+    }
+
+    public double[][] toArray() {
+        return null;
+    }
+
+    public Matrix transpose() {
+        return null;
+    }
+
+    public Matrix add(Matrix m2) {
+        return null;
+    }
+
+    public Matrix multiply(int i) {
+        return null;
+    }
+
+    public Matrix multiply(Matrix m4) {
+
+        return null;
+    }
+
+    // TODO: Add transpose, add, mutliply(double), multiply(Matrix)
+
+}
