65130f096d8e37b7379744f53d5ffe611a998164
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-30 23:30:45 +0800

    finish first attempt

--- src/main/java/idv/kuma/_4kyu/snail/Snail.java
+++ src/main/java/idv/kuma/_4kyu/snail/Snail.java
@@ -1,14 +1,88 @@
 package idv.kuma._4kyu.snail;
 
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 public class Snail {
+
+    private static Map<String, Point> directionToMovement = new HashMap<>();
+
+    static {
+        directionToMovement.put("Right", new Point(0, 1));
+        directionToMovement.put("Down", new Point(1, 0));
+        directionToMovement.put("Left", new Point(0, -1));
+        directionToMovement.put("Up", new Point(-1, 0));
+    }
 
 
     public static int[] snail(int[][] array) {
 
+        int n = array.length;
+        int numPoints = n * n;
+
+        List<Point> steppedPoints = new ArrayList();
+
+
+        // point (0,0) is stepped at beginning
+        String currentDirection = "Right";
+        Point currentPoint = new Point(0, 0);
+        steppedPoints.add(currentPoint);
+        System.out.println("Initial " + currentDirection + " -> " + currentPoint);
+
+        while (!allPointsAreStepped(steppedPoints, numPoints)) {
+
+            List<String> directions = getDirections(currentDirection);
+
+            for (String direction : directions) {
+
+                Point movement = directionToMovement.get(direction);
+                Point trialPoint = new Point(currentPoint.x + movement.x, currentPoint.y + movement.y);
+
+                if (goodToGo(trialPoint, n, steppedPoints)) {
+
+                    currentDirection = direction;
+                    currentPoint = trialPoint;
+                    steppedPoints.add(currentPoint);
+                    System.out.println("Moved " + currentDirection + " -> " + currentPoint);
+
+                    break;
+                }
+
+            }
+
+        }
 
 
         // enjoy
-        return new int[]{1};
+        return makeResult(array, steppedPoints);
+    }
+
+    private static int[] makeResult(int[][] array, List<Point> steppedPoints) {
+        return steppedPoints.stream().mapToInt(point -> array[point.x][point.y]).toArray();
+    }
+
+    private static boolean goodToGo(Point trialPoint, int n, List<Point> steppedPoints) {
+        // next still in map && next not stepped
+        if (trialPoint.x >= n || trialPoint.x < 0) return false;
+        if (trialPoint.y >= n || trialPoint.y < 0) return false;
+        if (steppedPoints.contains(trialPoint)) return false;
+
+        return true;
+
+    }
+
+    private static boolean allPointsAreStepped(final List<Point> steppedPoints, final int numPoints) {
+        return steppedPoints.size() >= numPoints;
+    }
+
+    private static List<String> getDirections(String currentDirection) {
+        if ("Right".equals(currentDirection)) return Arrays.asList("Right", "Down", "Left", "Up");
+        if ("Down".equals(currentDirection)) return Arrays.asList("Down", "Left", "Up", "Right");
+        if ("Left".equals(currentDirection)) return Arrays.asList("Left", "Up", "Right", "Down");
+        /*("Up".equals(currentDirection))*/
+        return Arrays.asList("Up", "Right", "Down", "Left");
     }
 //
 }
--- src/test/java/idv/kuma/_4kyu/snail/SnailTest.java
+++ src/test/java/idv/kuma/_4kyu/snail/SnailTest.java
@@ -19,9 +19,18 @@ public class SnailTest {
         test(array, expected);
     }
 
-    @Ignore
     @Test
-    public void SnailTest1() {
+    public void When_2_x_2() {
+        int[][] array
+                = {{1, 2},
+                {4, 3}};
+
+        int[] expected = {1, 2, 3, 4};
+        test(array, expected);
+    }
+
+    @Test
+    public void When_3_x_3_1() {
         int[][] array
                 = {{1, 2, 3},
                 {4, 5, 6},
@@ -31,16 +40,16 @@ public class SnailTest {
         test(array, expected);
     }
 
-//    @Test
-//    public void SnailTest2() {
-//        int[][] array
-//                = {{1, 3, 2},
-//                {4, 5, 6},
-//                {7, 8, 9}};
-//
-//        int[] expected = {1, 3, 2, 6, 9, 8, 7, 4, 5};
-//        test(array, expected);
-//    }
+    @Test
+    public void When_3_x_3_2() {
+        int[][] array
+                = {{1, 3, 2},
+                {4, 5, 6},
+                {7, 8, 9}};
+
+        int[] expected = {1, 3, 2, 6, 9, 8, 7, 4, 5};
+        test(array, expected);
+    }
 
     public void test(int[][] originalArray, int[] expecteds) {
         String text = "input:\n" + int2dToString(originalArray) + "\n\nexpected: \n" + Arrays.toString(expecteds);
