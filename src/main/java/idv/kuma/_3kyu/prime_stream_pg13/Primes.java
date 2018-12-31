5558a1e0c6bd682d7f08e0098d5bfeadd7111a1d
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-01 01:53:00 +0800

    basic cases ok. (but timeout on submission)

--- src/main/java/idv/kuma/_3kyu/prime_stream_pg13/Primes.java
+++ src/main/java/idv/kuma/_3kyu/prime_stream_pg13/Primes.java
@@ -1,24 +1,17 @@
 package idv.kuma._3kyu.prime_stream_pg13;
 
-import java.lang.instrument.Instrumentation;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class Primes {
 
-    private static List<Integer> foundPrimes = new ArrayList<>();
-
-    static {
-        foundPrimes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
-    }
 
     public static IntStream stream() {
 
+        return IntStream.iterate(2, i -> i + 1).filter(Primes::isPrime);
 
-        return  foundPrimes.stream().mapToInt(Integer::intValue);
+    }
 
-//        return IntStream.of(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71);
+    private static boolean isPrime(int number) {
+        return IntStream.rangeClosed(2, number / 2).noneMatch(i -> number % i == 0);
     }
 }
