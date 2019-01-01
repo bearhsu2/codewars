0368478c097b10d33d03e2071d265559cdff6131
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-01 22:38:30 +0800

    value ok

--- src/main/java/idv/kuma/_7kyu/benefactor/NewAverage.java
+++ src/main/java/idv/kuma/_7kyu/benefactor/NewAverage.java
@@ -1,10 +1,24 @@
 package idv.kuma._7kyu.benefactor;
 
-public class NewAverage
-{
-    public static long newAvg(double[] arr, double navg)
-    {
+import java.util.Arrays;
+
+public class NewAverage {
+    public static long newAvg(double[] arr, double navg) {
         // your code
-        return 0L;
+        int n = arr.length;
+
+        double sum = Arrays.stream(arr).sum();
+
+        double newSum = navg * (n + 1);
+
+
+        if (newSum <= sum) throw new IllegalArgumentException();
+
+        double diff = newSum - sum;
+
+        long result = (long) Math.ceil(diff);
+
+
+        return result;
     }
 }
--- src/test/java/idv/kuma/_7kyu/benefactor/NewAverageTest.java
+++ src/test/java/idv/kuma/_7kyu/benefactor/NewAverageTest.java
@@ -1,5 +1,6 @@
 package idv.kuma._7kyu.benefactor;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,11 +12,19 @@ public class NewAverageTest {
     }
 
     @Test
-    public void test() {
+    public void test1() throws Exception {
         System.out.println("Fixed Tests newAvg");
+
         double[] a = new double[]{14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 16.0};
         testing(NewAverage.newAvg(a, 90), 628);
-        a = new double[]{14, 30, 5, 7, 9, 11, 15};
+    }
+
+
+    @Test
+    public void test2() {
+
+
+        double[] a = new double[]{14, 30, 5, 7, 9, 11, 15};
         testing(NewAverage.newAvg(a, 92), 645);
     }
 
