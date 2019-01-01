18c64c6927daa9974b5a85f9d046069e93bd8b79
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-01 22:31:35 +0800

    init

---
+++ src/main/java/idv/kuma/_7kyu/benefactor/NewAverage.java
@@ -0,0 +1,10 @@
+package idv.kuma._7kyu.benefactor;
+
+public class NewAverage
+{
+    public static long newAvg(double[] arr, double navg)
+    {
+        // your code
+        return 0L;
+    }
+}
