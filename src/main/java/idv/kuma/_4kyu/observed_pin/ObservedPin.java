bc1c37485f0ba0253f8d2b2dbcfaedd403c6a942
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-02 23:48:19 +0800

    add static map

--- src/main/java/idv/kuma/_4kyu/observed_pin/ObservedPin.java
+++ src/main/java/idv/kuma/_4kyu/observed_pin/ObservedPin.java
@@ -1,9 +1,49 @@
 package idv.kuma._4kyu.observed_pin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class ObservedPin {
+
+
+    private static Map<Character, List<Integer>> observedToPossibles;
+
+    static {
+        observedToPossibles.put('1', Arrays.asList(1, 2, 4));
+        observedToPossibles.put('2', Arrays.asList(1, 2, 3, 5));
+        observedToPossibles.put('3', Arrays.asList(2, 3, 6));
+        observedToPossibles.put('4', Arrays.asList(1, 4, 5, 7));
+        observedToPossibles.put('5', Arrays.asList(2, 4, 5, 6, 8));
+        observedToPossibles.put('6', Arrays.asList(3, 5, 6, 9));
+        observedToPossibles.put('7', Arrays.asList(4, 7, 8));
+        observedToPossibles.put('8', Arrays.asList(5, 7, 8, 9, 0));
+        observedToPossibles.put('9', Arrays.asList(6, 8, 9));
+        observedToPossibles.put('0', Arrays.asList(0, 8));
+    }
+
     public static List<String> getPINs(String entered) {
+
+        char[] observedChars = entered.toCharArray();
+
+        List<List<Integer>> possiblesList = new ArrayList<>();
+
+        for (char observedChar : observedChars) {
+            possiblesList.add(observedToPossibles.get(observedChar));
+        }
+
+        generatePossiblePins(possiblesList);
+
+
         return null;
+    }
+
+    private static void generatePossiblePins(List<List<Integer>> possiblesList) {
+        if (possiblesList.size() == 1){
+
+        } else {
+            // recursive
+        }
     }
 }
--- src/test/java/idv/kuma/_4kyu/observed_pin/ObservedPinTest.java
+++ src/test/java/idv/kuma/_4kyu/observed_pin/ObservedPinTest.java
@@ -1,15 +1,16 @@
 package idv.kuma._4kyu.observed_pin;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
 public class ObservedPinTest {
 
+    private final static Comparator<String> comp = (s1, s2) -> s1.compareTo(s2);
     public static HashMap<String, String[]> expectations = new HashMap<String, String[]>() {
         {
             put("8", new String[]{"5", "7", "8", "9", "0"});
@@ -17,7 +18,6 @@ public class ObservedPinTest {
             put("369", new String[]{"236", "238", "239", "256", "258", "259", "266", "268", "269", "296", "298", "299", "336", "338", "339", "356", "358", "359", "366", "368", "369", "396", "398", "399", "636", "638", "639", "656", "658", "659", "666", "668", "669", "696", "698", "699"});
         }
     };
-    private final static Comparator<String> comp = (s1, s2) -> s1.compareTo(s2);
 
     @Test
     public void testPins() {
