6c6242cc07964a7f787b6306acd3d1b4f02c0365
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-05 21:17:00 +0800

    init

---
+++ src/main/java/idv/kuma/_4kyu/fuck_interpreter/Paintfuck.java
@@ -0,0 +1,11 @@
+package idv.kuma._4kyu.fuck_interpreter;
+
+
+public class Paintfuck {
+
+    public static String interpreter(String code, int iterations, int width, int height) {
+        // Implement your interpreter here
+
+        return  "";
+    }
+}
