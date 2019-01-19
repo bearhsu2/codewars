22ece3165d4a3483b369d679724908269dd92f76
Yu-Song Syu <kuma@simbo.com.tw>
2019-01-19 21:58:39 +0800

    format ok, test fails

--- src/main/java/idv/kuma/_5kyu/maximum_subarray_sum/Max.java
+++ src/main/java/idv/kuma/_5kyu/maximum_subarray_sum/Max.java
@@ -3,7 +3,7 @@ package idv.kuma._5kyu.maximum_subarray_sum;
 public class Max {
     public static int sequence(int[] array) {
 
-        
+
 
         return 0;
     }
--- src/test/java/idv/kuma/_5kyu/maximum_subarray_sum/MaxTest.java
+++ src/test/java/idv/kuma/_5kyu/maximum_subarray_sum/MaxTest.java
@@ -3,21 +3,24 @@ package idv.kuma._5kyu.maximum_subarray_sum;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MaxTest {
 
 
-
     @Test
     public void testEmptyArray() throws Exception {
-        assertEquals("Empty arrays should have a max of 0", 0, Max.sequence(new int[]{}));
+        runAndCheck(0);
     }
 
-    @Ignore
+
     @Test
     public void testExampleArray() throws Exception {
-        assertEquals("Example array should have a max of 6", 6, Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        runAndCheck(6, -2, 1, -3, 4, -1, 2, 1, -5, 4);
+    }
+
+    void runAndCheck(int max, int... ints) {
+        assertEquals("Empty arrays should have a max of " + max, max, Max.sequence(ints));
     }
 
 }
