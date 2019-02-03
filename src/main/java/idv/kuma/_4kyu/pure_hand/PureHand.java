18dfeed62dbc98485005eb1dc0c19742a1facaf2
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-03 15:16:35 +0800

    string to int list

--- src/main/java/idv/kuma/_4kyu/pure_hand/PureHand.java
+++ src/main/java/idv/kuma/_4kyu/pure_hand/PureHand.java
@@ -1,11 +1,41 @@
 package idv.kuma._4kyu.pure_hand;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class PureHand {
 
-    public static String solution(String tiles) {
-        // Your code here!
-        return null;
+    static EyesFinder EyesFinder = new EyesFinder();
+
+    public static String solution(String input) {
+
+
+        List<Integer> tiles = new ArrayList<>();
+
+
+        return "";
     }
+
+
+    static List<Integer> toIntegerList(String input) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (char c : input.toCharArray()) {
+            result.add(Integer.parseInt(String.valueOf(c)));
+        }
+
+
+        return result;
+    }
+
+    static class EyesFinder {
+
+
+        public void find(boolean add) {
+        }
+    }
+
+
 }
--- src/test/java/idv/kuma/_4kyu/pure_hand/PureHandTest.java
+++ src/test/java/idv/kuma/_4kyu/pure_hand/PureHandTest.java
@@ -1,6 +1,11 @@
 package idv.kuma._4kyu.pure_hand;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,5 +19,19 @@ public class PureHandTest {
     @Test
     public void sampleTest2() {
         assertEquals("258", PureHand.solution("1223334455678"));
+    }
+
+
+    @Test
+    public void ToIntegerList() throws Exception {
+
+        List<Integer> expected = new ArrayList<>(
+                Arrays.asList(1,2,2,3,3,3,4,4,5,5,6,7,8));
+
+        List<Integer> integers = PureHand.toIntegerList("1223334455678");
+
+
+        Assert.assertEquals(expected, integers);
+
     }
 }
