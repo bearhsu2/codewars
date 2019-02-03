c13450c86f533f8b72e9d7b7b797a51459a49c64
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-03 15:50:05 +0800

    find eyes ok

--- src/main/java/idv/kuma/_4kyu/pure_hand/PureHand.java
+++ src/main/java/idv/kuma/_4kyu/pure_hand/PureHand.java
@@ -2,19 +2,50 @@ package idv.kuma._4kyu.pure_hand;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PureHand {
 
-    static EyesFinder EyesFinder = new EyesFinder();
 
-    public static String solution(String input) {
-
-
-        List<Integer> tiles = new ArrayList<>();
+    public static String solution(String originalHand) {
 
 
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= 9; i++) {
+
+            List<Integer> tiles = toIntegerList(originalHand + i);
+
+            if (isWinning(tiles)) {
+                sb.append(String.valueOf(i));
+            }
+        }
+
+
+        return sb.toString();
+    }
+
+    private static boolean isWinning(List<Integer> tiles) {
+
+        List<Hand> candidateHands = findCandidateHands(tiles);
+
+        if (candidateHands.isEmpty()) return false;
+
+
+        return true;
+    }
+
+    static List<Hand> findCandidateHands(final List<Integer> tiles) {
+
+        List<Hand> hands = new ArrayList<>();
+
+        tiles.stream().filter(i -> Collections.frequency(tiles, i) > 1)
+                .collect(Collectors.toSet()).forEach(i -> hands.add(new Hand(i, tiles)));
+
+
+        return hands;
     }
 
 
@@ -30,12 +61,29 @@ public class PureHand {
         return result;
     }
 
-    static class EyesFinder {
 
+    static class Hand {
+        Integer eye;
+        List<Integer> remains;
 
-        public void find(boolean add) {
+        public Hand(Integer eye, List<Integer> tiles) {
+            this.eye = eye;
+
+            this.remains = ((List) ((ArrayList) tiles).clone());
+
+            for (int i = 0; i < 2; i++) {
+                this.remains.remove(eye);
+            }
+
+            remains.sort(Integer::compareTo);
+        }
+
+        public Integer getEye() {
+            return eye;
+        }
+
+        public List<Integer> getRemains() {
+            return remains;
         }
     }
-
-
 }
--- src/test/java/idv/kuma/_4kyu/pure_hand/PureHandTest.java
+++ src/test/java/idv/kuma/_4kyu/pure_hand/PureHandTest.java
@@ -26,7 +26,7 @@ public class PureHandTest {
     public void ToIntegerList() throws Exception {
 
         List<Integer> expected = new ArrayList<>(
-                Arrays.asList(1,2,2,3,3,3,4,4,5,5,6,7,8));
+                Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 7, 8));
 
         List<Integer> integers = PureHand.toIntegerList("1223334455678");
 
@@ -34,4 +34,45 @@ public class PureHandTest {
         Assert.assertEquals(expected, integers);
 
     }
+
+    @Test
+    public void When_Find_Eyes_For_12233344556788_Then_22_33_44_55_88() throws Exception {
+
+        List<Integer> tiles = new ArrayList<>(
+                Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 7, 8, 8));
+
+        List<PureHand.Hand> hands = PureHand.findCandidateHands(tiles);
+
+        Assert.assertNotNull(hands);
+        Assert.assertEquals(5, hands.size());
+        checkHand(hands.get(0), 2, Arrays.asList(1, 3, 3, 3, 4, 4, 5, 5, 6, 7, 8, 8));
+        checkHand(hands.get(1), 3, Arrays.asList(1, 2, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8));
+        checkHand(hands.get(2), 4, Arrays.asList(1, 2, 2, 3, 3, 3, 5, 5, 6, 7, 8, 8));
+        checkHand(hands.get(3), 5, Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 6, 7, 8, 8));
+        checkHand(hands.get(4), 8, Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 7));
+    }
+
+    @Test
+    public void When_Find_Eyes_For_12233344556782_Then_22_33_44_55() throws Exception {
+
+        List<Integer> tiles = new ArrayList<>(
+                Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 7, 8, 2));
+
+        List<PureHand.Hand> hands = PureHand.findCandidateHands(tiles);
+
+        Assert.assertNotNull(hands);
+        Assert.assertEquals(4, hands.size());
+        checkHand(hands.get(0), 2, Arrays.asList(1, 2, 3, 3, 3, 4, 4, 5, 5, 6, 7, 8));
+        checkHand(hands.get(1), 3, Arrays.asList(1, 2, 2, 2, 3, 4, 4, 5, 5, 6, 7, 8));
+        checkHand(hands.get(2), 4, Arrays.asList(1, 2, 2, 2, 3, 3, 3, 5, 5, 6, 7, 8));
+        checkHand(hands.get(3), 5, Arrays.asList(1, 2, 2, 2, 3, 3, 3, 4, 4, 6, 7, 8));
+    }
+
+    private void checkHand(PureHand.Hand hand, int eye, List<Integer> remains) {
+
+        Assert.assertEquals(eye, hand.getEye().intValue());
+        Assert.assertEquals(remains, hand.getRemains());
+    }
+
+
 }
