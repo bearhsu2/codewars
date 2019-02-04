2449f72bce3bb82a5cf954605d90245006c38a81
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-04 14:08:19 +0800

    refactor

--- src/main/java/idv/kuma/_5kyu/perimeter_of_squares_in_a_rectangle/SumFct.java
+++ src/main/java/idv/kuma/_5kyu/perimeter_of_squares_in_a_rectangle/SumFct.java
@@ -1,18 +1,17 @@
 package idv.kuma._5kyu.perimeter_of_squares_in_a_rectangle;
 
-
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class SumFct {
 
-    private static Map<BigInteger, BigInteger> nToFibonassi = new HashMap<>();
+    private static Map<BigInteger, BigInteger> nToFibonacci = new HashMap<>();
 
     static {
-        nToFibonassi.put(BigInteger.ZERO, BigInteger.ONE);
-        nToFibonassi.put(BigInteger.ONE, BigInteger.ONE);
-
+        nToFibonacci.put(BigInteger.ZERO, BigInteger.ONE);
+        nToFibonacci.put(BigInteger.ONE, BigInteger.ONE);
     }
 
     // https://www.codewars.com/kata/perimeter-of-squares-in-a-rectangle/train/java
@@ -21,21 +20,20 @@ public class SumFct {
         BigInteger result = BigInteger.ZERO;
 
         for (BigInteger i = BigInteger.ZERO; i.compareTo(n) <= 0; i = i.add(BigInteger.ONE)) {
-            BigInteger f = fibonassi(i);
-            result = result.add(f);
+            result = result.add(fibonacci(i));
         }
 
         return result.multiply(BigInteger.valueOf(4));
     }
 
-    private static BigInteger fibonassi(BigInteger n) {
-        if (n.equals(BigInteger.ZERO)) return BigInteger.ONE;
-        if (n.equals(BigInteger.ONE)) return BigInteger.ONE;
+    private static BigInteger fibonacci(BigInteger n) {
 
-        if (nToFibonassi.containsKey(n)) return nToFibonassi.get(n);
+        BigInteger existing = nToFibonacci.get(n);
 
-        BigInteger value = fibonassi(n.subtract(BigInteger.ONE)).add(fibonassi(n.subtract(BigInteger.valueOf(2))));
-        nToFibonassi.put(n, value);
+        if (!Objects.isNull(existing)) return existing;
+
+        BigInteger value = fibonacci(n.subtract(BigInteger.ONE)).add(fibonacci(n.subtract(BigInteger.valueOf(2))));
+        nToFibonacci.put(n, value);
 
         return value;
     }
