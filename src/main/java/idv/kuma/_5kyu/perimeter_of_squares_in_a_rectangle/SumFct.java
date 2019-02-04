96d0406610f066500e52da879de694cb154908fa
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-04 14:04:52 +0800

    speed ok

--- src/main/java/idv/kuma/_5kyu/perimeter_of_squares_in_a_rectangle/SumFct.java
+++ src/main/java/idv/kuma/_5kyu/perimeter_of_squares_in_a_rectangle/SumFct.java
@@ -2,8 +2,18 @@ package idv.kuma._5kyu.perimeter_of_squares_in_a_rectangle;
 
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SumFct {
+
+    private static Map<BigInteger, BigInteger> nToFibonassi = new HashMap<>();
+
+    static {
+        nToFibonassi.put(BigInteger.ZERO, BigInteger.ONE);
+        nToFibonassi.put(BigInteger.ONE, BigInteger.ONE);
+
+    }
 
     // https://www.codewars.com/kata/perimeter-of-squares-in-a-rectangle/train/java
     public static BigInteger perimeter(BigInteger n) {
@@ -21,7 +31,13 @@ public class SumFct {
     private static BigInteger fibonassi(BigInteger n) {
         if (n.equals(BigInteger.ZERO)) return BigInteger.ONE;
         if (n.equals(BigInteger.ONE)) return BigInteger.ONE;
-        return fibonassi(n.subtract(BigInteger.ONE)).add(fibonassi(n.subtract(BigInteger.valueOf(2))));
+
+        if (nToFibonassi.containsKey(n)) return nToFibonassi.get(n);
+
+        BigInteger value = fibonassi(n.subtract(BigInteger.ONE)).add(fibonassi(n.subtract(BigInteger.valueOf(2))));
+        nToFibonassi.put(n, value);
+
+        return value;
     }
 
 }
