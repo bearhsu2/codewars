65e22da2e568519b4cedf15b06726d05d195213b
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-04 13:35:38 +0800

    init

---
+++ src/main/java/idv/kuma/_5kyu/perimeter_of_squares_in_a_rectangle/SumFct.java
@@ -0,0 +1,14 @@
+package idv.kuma._5kyu.perimeter_of_squares_in_a_rectangle;
+
+
+import java.math.BigInteger;
+
+public class SumFct {
+
+    // https://www.codewars.com/kata/perimeter-of-squares-in-a-rectangle/train/java
+    public static BigInteger perimeter(BigInteger n) {
+        // your code
+
+        return BigInteger.valueOf(0L);
+    }
+}
