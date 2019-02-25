459244f56411e70e6efd2b1210d1d42a156c1666
Yu-Song Syu <kuma@simbo.com.tw>
2019-02-25 20:44:42 +0800

    init: failed test

---
+++ src/main/java/idv/kuma/_3kyu/closest_pair_of_points_in_linearithmic_time/Kata.java
@@ -0,0 +1,11 @@
+package idv.kuma._3kyu.closest_pair_of_points_in_linearithmic_time;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Kata {
+
+    public static List<Point> closestPair(List<Point> points) {
+        return Arrays.asList();
+    }
+}
