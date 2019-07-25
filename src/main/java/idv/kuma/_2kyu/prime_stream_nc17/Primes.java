9f5be13a16801085e1872bfb2b192721e177fadd
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-25 23:58:09 +0800

    use stream to write

--- src/main/java/idv/kuma/_2kyu/prime_stream_nc17/Primes.java
+++ src/main/java/idv/kuma/_2kyu/prime_stream_nc17/Primes.java
@@ -4,33 +4,23 @@ import java.util.stream.IntStream;
 
 public class Primes {
 
-    private static final int N = 2_000_000_000;
-
-    // true: not prime
-    // false: prime
-    private static boolean[] SIEVE = new boolean[N + 1];
-
-    static {
-        SIEVE[0] = true;
-        SIEVE[1] = true;
-
-        for (int i = 2; i * i <= N; i++) {
-            if (!SIEVE[i]) {
-                for (int j = i; i * j <= N; j++) {
-                    SIEVE[i * j] = true;
-                }
-            }
-        }
-    }
-
 
     public static IntStream stream() {
 
         IntStream stream1 = IntStream.builder().add(2).build();
 
-        IntStream stream2 = IntStream.iterate(3, i -> i + 2).filter(i -> !SIEVE[i]);
+        IntStream stream2 = IntStream.iterate(3, i -> i + 2).filter(Primes::isPrime);
 
         return IntStream.concat(stream1, stream2);
+
+    }
+
+
+    private static boolean isPrime(int candidate) {
+
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return IntStream.rangeClosed(2, candidateRoot)
+                .noneMatch(i -> candidate % i == 0);
 
     }
 
