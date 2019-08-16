2ff5bb563f6f7c92e43e8a342d9be14c16ebc3dc
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-16 22:12:35 +0800

    1, 2

---
+++ src/main/java/idv/kuma/_3kyu/millionth_fibonacci.java
@@ -0,0 +1,4 @@
+package idv.kuma._3kyu;
+
+public class millionth_fibonacci {
+}
---
+++ src/main/java/idv/kuma/_3kyu/the_millionth_fibonacci_kata/Fibonacci.java
@@ -0,0 +1,12 @@
+package idv.kuma._3kyu.the_millionth_fibonacci_kata;
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+
+    public static BigInteger fib(BigInteger n) {
+        // ...
+        return BigInteger.ONE;
+    }
+
+}
