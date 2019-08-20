2de1bf1fc954fbdbaf09b9f6c8e6ab09b4abd115
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-20 17:56:17 +0800

    init classes

---
+++ src/main/java/idv/kuma/_6kyu/decode_thie_morse_code/MorseCodeDecoder.java
@@ -0,0 +1,4 @@
+package idv.kuma._6kyu.decode_thie_morse_code;
+
+public class MorseCodeDecoder {
+}
---
+++ src/test/java/idv/kuma/_6kyu/decode_thie_morse_code/MorseCodeDecoderTest.java
@@ -0,0 +1,7 @@
+package idv.kuma._6kyu.decode_thie_morse_code;
+
+import static org.junit.Assert.*;
+
+public class MorseCodeDecoderTest {
+
+}
