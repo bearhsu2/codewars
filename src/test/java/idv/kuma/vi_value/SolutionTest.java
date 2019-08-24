85b3bb754afaeb78cc2bd5590833440d2a6a4ae2
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-24 17:07:47 +0800

    add tests

--- src/test/java/idv/kuma/vi_value/SolutionTest.java
+++ src/test/java/idv/kuma/vi_value/SolutionTest.java
@@ -1,6 +1,9 @@
 package idv.kuma.vi_value;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,5 +21,32 @@ public class SolutionTest {
     public void test2() {
 
         assertEquals(2, new Solution().solution(new int[]{4, 4}));
+    }
+
+
+    @Test
+    public void all_different() {
+
+        assertEquals(2, new Solution().solution(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void all_same() {
+
+        assertEquals(5, new Solution().solution(new int[]{1, 1, 1, 1, 1}));
+    }
+
+
+    @Ignore
+    @Test
+    public void long_array() {
+
+        int[] longArray = IntStream
+                .rangeClosed(1, 1_000_000)
+                .boxed()
+                .mapToInt(value -> value.intValue())
+                .toArray();
+
+        assertEquals(2, new Solution().solution(longArray));
     }
 }
