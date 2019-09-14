07ac1f23ba21cfb4ae9ac3cb9c0f4f6b3720df55
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-14 15:26:42 +0800

    refactor array invert

--- javascript/_8kyu/invert_values/invert.js
+++ javascript/_8kyu/invert_values/invert.js
@@ -1,12 +1,8 @@
 function invert(array) {
 
-    const result = [];
+    return array.map(element => element * -1);;
 
-    for (let i = 0; i < array.length; i++) {
-        result.push(array[i] * -1);
-    }
 
-    return result;
 }
 
 module.exports = invert;
