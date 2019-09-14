91c7aebb57f8b77b9354c07e70f3ffc993f9746e
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-14 15:54:52 +0800

    remove unused comments

--- javascript/_8kyu/correct_the_mistakes_of_the_character_recognition_software/correct.test.js
+++ javascript/_8kyu/correct_the_mistakes_of_the_character_recognition_software/correct.test.js
@@ -11,8 +11,3 @@ test('1 -> I', () => {
     expect(correct('51NGAP0RE')).toBe('SINGAPORE');
 });
 
-// Test.assertEquals(correct("L0ND0N"),"LONDON");
-// Test.assertEquals(correct("DUBL1N"),"DUBLIN");
-// Test.assertEquals(correct("51NGAP0RE"),"SINGAPORE");
-// Test.assertEquals(correct("BUDAPE5T"),"BUDAPEST");
-// Test.assertEquals(correct("PAR15"),"PARIS");
