31faca9715203c25bc7defcb21157d03f0cbf967
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-14 16:20:13 +0800

    ok

--- javascript/_8kyu/count_of_positives_sum_of_negatives/countPositivesSumNegatives.js
+++ javascript/_8kyu/count_of_positives_sum_of_negatives/countPositivesSumNegatives.js
@@ -1,5 +1,24 @@
 function countPositivesSumNegatives(input) {
-    return [];
+    if (input.length === 0) {
+        return [];
+    }
+
+    let count = 0;
+    let sum = 0;
+
+    for (let i = 0; i < input.length; i++) {
+
+        const number = input[i];
+
+        if (number > 0) {
+            count++;
+        } else if (number < 0){
+            sum += number;
+        }
+
+    }
+
+    return [count, sum];
 }
 
 module.exports = countPositivesSumNegatives;
