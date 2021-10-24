9662ebd6328e19cd1f38faadeb1fc697908f3e94
Yu-Song Syu <bearhsu2@gmail.com>
2021-10-24 23:42:45 +0800

    ok

--- java/src/main/java/idv/kuma/_3kyu/rail_fence_cipher_encoding_and_decoding/RailFenceCipher.java
+++ java/src/main/java/idv/kuma/_3kyu/rail_fence_cipher_encoding_and_decoding/RailFenceCipher.java
@@ -1,6 +1,6 @@
 package idv.kuma._3kyu.rail_fence_cipher_encoding_and_decoding;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class RailFenceCipher {
@@ -35,12 +35,55 @@ public class RailFenceCipher {
         }
 
         // Your code here
-        String result = Arrays.stream(shards).collect(Collectors.joining());
-        return result;
+        return Arrays.stream(shards).collect(Collectors.joining());
     }
 
     static String decode(String s, int n) {
         // Your code here
-        return "";
+        char[] chars = s.toCharArray();
+        List<Integer>[] originalIndicesArray = new List[n];
+        for (int i = 0; i < n; i++) {
+            originalIndicesArray[i] = new ArrayList<>();
+        }
+
+
+        int shardIndex = 0;
+        int direction = 1;
+        for (int original = 0; original < chars.length; original++) {
+
+            originalIndicesArray[shardIndex].add(original);
+
+            shardIndex += direction;
+
+            if (shardIndex >= n) {
+                shardIndex -= 2;
+                direction = -1;
+            }
+
+            if (shardIndex < 0) {
+                shardIndex += 2;
+                direction = 1;
+            }
+
+        }
+
+        Map<Integer, Integer> encodedToOriginalIndex = new HashMap<>();
+        int encodedIndex = 0;
+        for (List<Integer> originalIndices : originalIndicesArray) {
+            for (Integer originalIndex : originalIndices) {
+                encodedToOriginalIndex.put(encodedIndex, originalIndex);
+                encodedIndex++;
+            }
+        }
+
+        char[] decodesChars = new char[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            int originalIndex = encodedToOriginalIndex.get(i);
+            decodesChars[originalIndex] = s.charAt(i);
+        }
+
+
+
+        return String.valueOf(decodesChars);
     }
 }
