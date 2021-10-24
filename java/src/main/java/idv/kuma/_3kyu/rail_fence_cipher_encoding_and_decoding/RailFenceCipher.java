e3dbe8f20cce50d03f2440c32f48e6c7d11bc5a6
Yu-Song Syu <bearhsu2@gmail.com>
2021-10-24 22:51:34 +0800

    basic form (not pass tests yet)

---
+++ java/src/main/java/idv/kuma/_3kyu/rail_fence_cipher_encoding_and_decoding/RailFenceCipher.java
@@ -0,0 +1,14 @@
+package idv.kuma._3kyu.rail_fence_cipher_encoding_and_decoding;
+
+public class RailFenceCipher {
+
+    static String encode(String s, int n) {
+        // Your code here
+        return null;
+    }
+
+    static String decode(String s, int n) {
+        // Your code here
+        return null;
+    }
+}
