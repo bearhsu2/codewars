703dfaf8195578fd94a344a812dc320a1dc51a48
Yu-Song Syu <bearhsu2@gmail.com>
2021-10-24 23:04:35 +0800

    encode

--- java/src/main/java/idv/kuma/_3kyu/rail_fence_cipher_encoding_and_decoding/RailFenceCipher.java
+++ java/src/main/java/idv/kuma/_3kyu/rail_fence_cipher_encoding_and_decoding/RailFenceCipher.java
@@ -1,14 +1,46 @@
 package idv.kuma._3kyu.rail_fence_cipher_encoding_and_decoding;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class RailFenceCipher {
 
     static String encode(String s, int n) {
+        char[] chars = s.toCharArray();
+        String[] shards = new String[n];
+        for (int i = 0; i < n; i++) {
+            shards[i] = "";
+        }
+
+        int index = 0;
+        int direction = 1;
+        for (int i = 0; i < chars.length; i++) {
+
+            char aChar = chars[i];
+            shards[index] += aChar;
+
+            index += direction;
+
+            if (index >= n) {
+                index -= 2;
+                direction = -1;
+            }
+
+            if (index < 0) {
+                index += 2;
+                direction = 1;
+            }
+
+
+        }
+
         // Your code here
-        return null;
+        String result = Arrays.stream(shards).collect(Collectors.joining());
+        return result;
     }
 
     static String decode(String s, int n) {
         // Your code here
-        return null;
+        return "";
     }
 }
